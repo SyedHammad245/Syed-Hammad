524967590f155f2b7fcb6e86a615d2a24a749452
SP22-BSCS-0023 <SP22-BSCS-0023@MJDT-0217>
2024-03-22 12:33:09 +0500

    Add project files.

---
+++ Syed Hammad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Syed_Hammad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
